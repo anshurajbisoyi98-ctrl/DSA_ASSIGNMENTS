fa7a24f1d255deef7b8512472b5265fe476934da
anshurajbisoyi98-ctrl <anshurajbisoyi98@gmail.com>
2026-06-10 11:41:13 +0530

    Add files via upload

---
+++ prevsmaller.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+vector<int> arr(n);
+for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+vector<int>ans(n);
+ans[0]=-1;
+stack<int>st;
+st.push(arr[0]);
+for(int i=1;i<n;i++){
+    while (!st.empty()&&st.top()>=arr[i])
+    {
+    st.pop();
+    }
+    if (st.empty())
+    {
+        ans[i]=-1;
+    }
+    else{ans[i]=st.top();
+    }
+    st.push(arr[i]);
+}
+    for (int i = 0; i < n; i++)
+    {
+    cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+
+}
+
+  
+
+
+  
